5dbf264f7fb675f27ad0cad8abedc0576cb8ea3f
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-14 14:29:46 +0100

    moved querying of dom for #content from renderHome to index.js, so we dont query the dom everytime renderHome runs. following render functions will all take a element parameter that they can inject their content into, making it more modular

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import renderHome from "./renderHome.js";
 
-renderHome();
+const main = document.querySelector("#content");
+
+renderHome(main);
--- src/renderHome.js
+++ src/renderHome.js
@@ -1,19 +1,17 @@
 import "./style.css";
 import heroImg from "./images/treehouse.png";
 
-function renderHome() {
-  const content = document.querySelector("#content");
-
+function renderHome(element) {
   const heading = document.createElement("h1");
   heading.textContent = `Andy's Acorngasmic Appetizers`;
   heading.classList.add("red-heading");
 
-  content.appendChild(heading);
+  element.appendChild(heading);
 
   const img = document.createElement("img");
   img.src = heroImg;
 
-  content.appendChild(img);
+  element.appendChild(img);
 }
 
 export default renderHome;
