68d6208c11ae93544b9fda5bc618556964462229
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-14 14:55:57 +0100

    add css layer, css-components folder for each css component

--- src/renderHome.js
+++ src/renderHome.js
@@ -1,17 +1,22 @@
 import "./style.css";
-import heroImg from "./images/treehouse.png";
+import treehouseImg from "./images/treehouse.png";
 
 function renderHome(element) {
+  //each render function will use a makeHeader function
   const heading = document.createElement("h1");
   heading.textContent = `Andy's Acorngasmic Appetizers`;
   heading.classList.add("red-heading");
-
+  //
   element.appendChild(heading);
 
+  const reviewArticle = document.createElement("article");
   const img = document.createElement("img");
-  img.src = heroImg;
+  img.src = treehouseImg;
+  img.classList.add("treehouse-img");
 
-  element.appendChild(img);
+  reviewArticle.appendChild(img);
+
+  element.appendChild(reviewArticle);
 }
 
 export default renderHome;
