121942c0c6cf48f24b328dfee19992fe22068b1d
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-14 13:27:02 +0100

    add style.css, install style-loader & css-loader, configured webpack to use it

--- src/pageload.js
+++ src/pageload.js
@@ -1,8 +1,11 @@
+import "./style.css";
+
 function pageLoad() {
   const content = document.querySelector("#content");
 
   const heading = document.createElement("h1");
   heading.textContent = `Mom's Outback Spaghetti-o's`;
+  heading.classList.add("red-heading");
 
   content.appendChild(heading);
 }
