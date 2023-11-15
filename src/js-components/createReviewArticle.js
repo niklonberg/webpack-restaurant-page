1910c84f2041b088b0ff5e74d862cbd648673b6e
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-15 10:32:53 +0100

    implement base create review article module

---
+++ src/js-components/createReviewArticle.js
@@ -0,0 +1,25 @@
+function createReviewArticle(author, image = null) {
+  const reviewArticle = document.createElement("article");
+
+  const img = document.createElement("img");
+  img.src = image;
+  img.classList.add("treehouse-img");
+  reviewArticle.appendChild(img);
+
+  const p = document.createElement("p");
+  p.textContent = `Andy's with their own homebrewed syrup has the best pancakes around! 
+    The atmosphere and customer service make you feel like you are sitting
+    in the middle of the woods, gorging on nuts like a squirrel! 
+    This is exactly the kind of place that I like to return to again and again.`;
+  reviewArticle.appendChild(p);
+
+  const footer = document.createElement("footer");
+  const cite = document.createElement("cite");
+  cite.textContent = "Goldilocks";
+  footer.appendChild(cite);
+  reviewArticle.appendChild(footer);
+
+  return reviewArticle;
+}
+
+export default createReviewArticle;
--- src/js-components/renderHome.js
+++ src/js-components/renderHome.js
@@ -1,18 +1,13 @@
 import "../style.css";
 import treehouseImg from "../images/treehouse.png";
 import createHeading from "./createHeading.js";
+import createReviewArticle from "./createReviewArticle.js";
 
 function renderHome(element) {
   const heading = createHeading("Andy's Acorntastic Appetizers");
   element.appendChild(heading);
 
-  const reviewArticle = document.createElement("article");
-  const img = document.createElement("img");
-  img.src = treehouseImg;
-  img.classList.add("treehouse-img");
-
-  reviewArticle.appendChild(img);
-
+  const reviewArticle = createReviewArticle("Adam", treehouseImg);
   element.appendChild(reviewArticle);
 }
 
