f097095a98e33569d1b71d34e9eceb23c54a8c0b
Nikolai Lønberg <niklonberg@gmail.com>
2023-11-16 10:30:14 +0100

    set up base render menu file

---
+++ src/js-components/renderMenu.js
@@ -0,0 +1,8 @@
+import "../style.css";
+import treehouseImg from "../images/treehouse.png";
+import ElementFactory from "./ElementFactory.js";
+import createDescList from "./createDescList.js";
+
+function renderMenu() {}
+
+export default renderMenu;
